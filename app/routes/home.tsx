6c1aea4ca235c86719ec96635d3326a4c67f58b7
darkroom-debug <satyam.bscitstudent23.9632@cimage.in>
2025-11-22 05:47:24 +0530

    front end page done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-no-repeat bg-center">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+      <h1>Track Your Application & Resume Rating</h1>
+      <h2>Review Your submission and check AI-Powerd feedback.</h2>
+      </div>
+    {/* resume loding part */}
+    {resumes.length >0 &&(
+       <div className="resumes-section">
+      {resumes.map((resume)=>(
+      <ResumeCard key={resume.id} resume={resume}  />
+    ))}
+    </div>
+
+    )}
+
+    </section>
+
+  </main>
 }
